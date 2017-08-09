a6f4c061ade3178d2fbbf0fa7c1af652e13ccd01
Maxime GIBSON <mgibson@e3r1p6.42.fr>
2017-08-09 14:14:48 +0200

    Day02 - ex01 modification

--- Day02/ex01/ft_print_reverse_alphabet.c
+++ Day02/ex01/ft_print_reverse_alphabet.c
@@ -6,28 +6,18 @@
 /*   By: mgibson <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/08/09 13:19:48 by mgibson           #+#    #+#             */
-/*   Updated: 2017/08/09 13:38:11 by mgibson          ###   ########.fr       */
+/*   Updated: 2017/08/09 14:08:17 by mgibson          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-void	ft_putchar(char c)
-{
-	write(1,&c,1);
-}
-
 void	ft_print_reverse_alphabet(void)
 {
-	int n = 'z';
+	int n;
 
-	while(n >= 'a')
+	n = 'z';
+	while (n >= 'a')
 	{
 		ft_putchar(n);
 		n--;
 	}
-}
-
-int	main()
-{
-	ft_print_reverse_alphabet();
-	return(0);
 }
