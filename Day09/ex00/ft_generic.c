858de4d0e7203e0b02e2e79d5632f74a91d3d9b9
Maxime GIBSON <mgibson@e1r3p11.42.fr>
2017-08-10 17:44:24 +0200

    ex00 normized - Day09

--- Day09/ex00/ft_generic.c
+++ Day09/ex00/ft_generic.c
@@ -6,20 +6,11 @@
 /*   By: mgibson <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/08/10 16:56:50 by mgibson           #+#    #+#             */
-/*   Updated: 2017/08/10 17:39:09 by mgibson          ###   ########.fr       */
+/*   Updated: 2017/08/10 17:42:17 by mgibson          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-#include <unistd.h>
-
-void	ft_putchar(char c);
 
 void	ft_generic(void)
 {
 	write(1, "Tut tut ; Tut tut\n", 18);
-}
-
-int	main()
-{
-	ft_generic();
-	return(0);
 }
