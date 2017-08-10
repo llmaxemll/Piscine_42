e35cf9627b40dbc617d2e7f308d3142c1ebd421b
Maxime GIBSON <mgibson@e1r3p11.42.fr>
2017-08-10 17:21:47 +0200

    Add Day9 - ex00

---
+++ Day09/ex00/ft_generic.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_generic.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mgibson <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/08/10 16:56:50 by mgibson           #+#    #+#             */
+/*   Updated: 2017/08/10 17:20:54 by mgibson          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	ft_generic(void)
+{
+	ft_putchar('T');
+	ft_putchar('u');
+	ft_putchar('t');
+	ft_putchar(' ');
+	ft_putchar('t');
+	ft_putchar('u');
+	ft_putchar('t');
+	ft_putchar(' ');
+	ft_putchar(';');
+	ft_putchar(' ');
+	ft_putchar('T');
+	ft_putchar('u');
+	ft_putchar('t');
+	ft_putchar(' ');
+	ft_putchar('t');
+	ft_putchar('u');
+	ft_putchar('t');
+}
