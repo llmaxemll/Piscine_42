ae74bb1d9d514a21deb494c0c71b8d4f5d9ca2ac
Maxime GIBSON <mgibson@e3r3p1.42.fr>
2017-08-11 15:50:35 +0200

    ex01 modification - Day09

--- Day09/ex01/ft_takes_place.c
+++ Day09/ex01/ft_takes_place.c
@@ -6,7 +6,7 @@
 /*   By: mgibson <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/08/10 17:56:29 by mgibson           #+#    #+#             */
-/*   Updated: 2017/08/10 23:26:13 by mgibson          ###   ########.fr       */
+/*   Updated: 2017/08/11 15:47:52 by mgibson          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@ void	ft_why(int hour)
 			hour = hour - 12;
 			y = y - 24;
 			printf("THE FOLLOWING TAKES PLACE BETWEEN ");
-			printf("%d.00 P.M. AND %d.00 A.M.\n", hour, y);
+			printf("%d.00 A.M. AND %d.00 A.M.\n", hour, y);
 		}
 		else
 		{
@@ -42,11 +42,11 @@ void	ft_bruh(int hour)
 	int y;
 
 	y = hour + 1;
-	if (hour > 12 && y < 12)
+	if (hour == 12)
 	{
-		hour = hour - 12;
+		y = y - 12;
 		printf("THE FOLLOWING TAKES PLACE BETWEEN ");
-		printf("%d.00 P.M. AND %d.00 A.M.\n", hour, y);
+		printf("%d.00 P.M. AND %d.00 P.M.\n", hour, y);
 	}
 	else
 	{
@@ -65,7 +65,7 @@ void	ft_takes_place(int hour)
 		{
 			hour = hour + 12;
 			printf("THE FOLLOWING TAKES PLACE BETWEEN ");
-			printf("%d.00 P.M. AND %d.00 A.M.\n", hour, y);
+			printf("%d.00 A.M. AND %d.00 A.M.\n", hour, y);
 		}
 		else
 		{
